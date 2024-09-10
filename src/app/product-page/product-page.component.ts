fa75d5d293d989923d3af6aa1be393b0c71c492c
janith <mihiranga.office@gmail.com>
2024-09-10 17:09:10 +0530

    boostarap and some new features

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,15 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,FormsModule,RouterLink],
+  imports: [CommonModule,RouterOutlet,FormsModule,RouterLink,ReactiveFormsModule],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss'
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit{
+ 
+  newProductForm!:FormGroup;
+  constructor(private addProduct:FormBuilder){
+
+  }
+ 
+ 
+  ngOnInit(): void {
+    this.newProductForm=this.addProduct.group({
+      
+      productName:['',Validators.required],
+      enterPrice:['',Validators.required],
+      manufacturingDate:['',Validators.required],
+      expirationDate:['',Validators.required],
+      addImage:[''],
+    })
+   
+  }
+
 
 }
